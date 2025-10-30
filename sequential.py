8532f8d0531f98b38e010809a86181f3404dc657
mayjary <mayankpoojary123@gmail.com>
2025-10-30 20:40:04 +0530

    done2

--- sequential.py
+++ sequential.py
@@ -1,51 +1,14 @@
-def sequential_file_allocation(disk, file_name, start, length):
-    n = len(disk)
-
-    # Check bounds
-    if start + length > n:
-        print(f"Cannot allocate {file_name}: Out of disk bounds.")
-        return
-
-    # Check if blocks are free
-    for i in range(start, start + length):
-        if disk[i] != -1:
-            print(f"Cannot allocate {file_name}: Block {i} already allocated.")
+def sequential_file_allocation(start_block, file_size, disk_blocks):
+    allocated = []
+    for i in range(start_block, start_block + file_size):
+        if i >= len(disk_blocks) :
+            print("File cannot be allocated sequentially.")
             return
-
-    # Allocate file
-    for i in range(start, start + length):
-        disk[i] = file_name
-    print(f"File '{file_name}' allocated from Block {start} to Block {start + length - 1}.")
-
-
-def display_disk(disk):
-    print("\nDisk Status:")
-    for i in range(len(disk)):
-        print(f"Block {i}: {disk[i]}")
-    print("-" * 40)
+        allocated.append(i)
+    for block in allocated:
+        disk_blocks[block] = 1
+    print("File allocated at blocks:", allocated)
 
 
-# Main program
-if __name__ == "__main__":
-    size = int(input("Enter total number of disk blocks: "))
-    disk = [-1] * size
-
-    while True:
-        print("\nMenu: 1. Allocate File  2. Display Disk  3. Exit")
-        choice = int(input("Enter choice: "))
-
-        if choice == 1:
-            file_name = input("Enter file name: ")
-            start = int(input("Enter starting block: "))
-            length = int(input("Enter length of file (in blocks): "))
-            sequential_file_allocation(disk, file_name, start, length)
-
-        elif choice == 2:
-            display_disk(disk)
-
-        elif choice == 3:
-            print("Exiting program.")
-            break
-
-        else:
-            print("Invalid choice. Try again.")
+disk = [0] * 20  
+sequential_file_allocation(5,4,disk)
